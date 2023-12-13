1bbed2abebdd0e1c8d074e448a0f870927183d49
SSB4455 <741086@gmail.com>
2023-12-13 15:22:34 +0800

    调整操作UE

--- mklinkGUI/Form1.cs
+++ mklinkGUI/Form1.cs
@@ -9,6 +9,10 @@ namespace mklinkGUI
 {
 	public partial class Form1 : Form
 	{
+		string sourceFolder = "";
+
+
+
 		public Form1()
 		{
 			InitializeComponent();
@@ -22,35 +26,107 @@ namespace mklinkGUI
 
 			if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
 			{
-				string selectedFolder = folderBrowserDialog.SelectedPath;
+				sourceFolder = folderBrowserDialog.SelectedPath;
 				//MessageBox.Show("您选择的网络文件夹是：" + selectedFolder);
 				// 在这里使用 selectedFolder，进行您需要的操作
-				textBox1.Text = selectedFolder;
+				textBox1.Text = sourceFolder;
 			}
+		}
+
+		private void radioButton1_CheckedChanged(object sender, EventArgs e)
+		{
+			RadioButton radioButton = sender as RadioButton;
+			if (radioButton != null && radioButton.Checked)
+			{
+				sourceFolder = @"\\" + radioButton.Text + Path.DirectorySeparatorChar;
+
+				VistaFolderBrowserDialog folderBrowserDialog = new VistaFolderBrowserDialog();
+				folderBrowserDialog.Reset();
+				folderBrowserDialog.Description = "选择源目录";
+				folderBrowserDialog.UseDescriptionForTitle = true;
+				folderBrowserDialog.SelectedPath = sourceFolder;
+
+				if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+				{
+					sourceFolder = folderBrowserDialog.SelectedPath;
+					//MessageBox.Show("您选择的网络文件夹是：" + selectedFolder);
+				}
+			}
+		}
+
+		private void textBox1_OnKeyDownHandler(object sender, KeyEventArgs e)
+		{
+			if (e.KeyCode == Keys.Enter)
+			{
+				radioButton5.Checked = false;
+				radioButton5.Checked = true;
+			}
+		}
+
+		private void radioButton5_CheckedChanged(object sender, EventArgs e)
+		{
+			sourceFolder = textBox1.Text;
 		}
 
 		private void button2_Click(object sender, EventArgs e)
 		{
+			if (string.IsNullOrEmpty(sourceFolder))
+			{
+				MessageBox.Show("请选择源目录", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				return;
+			}
 			FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
 			folderBrowserDialog.Description = "选择要映射到的本地目录";
 			folderBrowserDialog.RootFolder = Environment.SpecialFolder.MyComputer;
 
 			if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
 			{
-				string selectedFolder = folderBrowserDialog.SelectedPath;
+				string targetFolder = folderBrowserDialog.SelectedPath;
 				//MessageBox.Show("You selected: " + selectedFolder);
-				textBox2.Text = selectedFolder;
 
-				if (!Directory.Exists(selectedFolder))
+				if (!targetFolder.EndsWith(Path.DirectorySeparatorChar.ToString()))
 				{
-					MessageBox.Show("“" + selectedFolder + "”目录不存在");
+					targetFolder += Path.DirectorySeparatorChar;
+				}
+				targetFolder += GetFolderName(sourceFolder);
+				string outputError = RunCmd("mklink /d " + targetFolder + " " + sourceFolder);
+				if (!string.IsNullOrEmpty(outputError))
+				{
+					MessageBox.Show(outputError, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
 					return;
 				}
-				string targetFolder = selectedFolder + Path.DirectorySeparatorChar + new DirectoryInfo(textBox1.Text).Name;
-				string output = RunCmd("mklink /d " + targetFolder + " " + textBox1.Text);
-				label3.Text = output;
 				Process.Start(targetFolder);
 			}
+		}
+
+		string GetFolderName(string pathStr)
+		{
+			string folderName = "";
+			try {
+				if (Directory.Exists(pathStr))
+				{
+					folderName = new DirectoryInfo(pathStr).Name;
+				}
+			}
+			catch { }
+
+			if (string.IsNullOrEmpty(folderName))
+			{
+				folderName = pathStr;
+				int startIndex = pathStr.LastIndexOf(Path.DirectorySeparatorChar);
+				int endIndex = startIndex;
+				if (pathStr.Split(Path.DirectorySeparatorChar).Length > 2 && endIndex > 0)
+				{
+					startIndex = pathStr.LastIndexOf(Path.DirectorySeparatorChar, endIndex - 1) + 1;
+				}
+				else
+				{
+					endIndex = pathStr.Length - 1;
+				}
+				folderName = pathStr.Substring(startIndex, endIndex - startIndex);
+			}
+			folderName = folderName.Replace(":", "").Replace(";", "").Replace(Path.DirectorySeparatorChar, '-');
+			return folderName;
 		}
 
 		string RunCmd(string cmd)
@@ -103,7 +179,8 @@ namespace mklinkGUI
 			process.WaitForExit();
 			process.Close();
 
-			return SplitCmdCurrentPath(output) + error;
+			//return SplitCmdCurrentPath(output) + error;
+			return error;
 		}
 
 		string SplitCmdCurrentPath(string output)
